3dba566025f3cfd117cd5fb322ae8d50bba96b03
Clément Causer <ccauser@simplonprod.co>
2022-09-25 18:26:12 +0200

    🚀  Setup Redux-Toolkit

---
+++ src/store/index.ts
@@ -0,0 +1,20 @@
+import {
+  useDispatch as useDispatchBase,
+  useSelector as useSelectorBase,
+} from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: {},
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+
+// Inferred type: { users: UsersState}
+type AppDispatch = typeof store.dispatch;
+
+export const useDispatch = () => useDispatchBase<AppDispatch>();
+export const useSelector = <TSelected = unknown>(
+  selector: (state: RootState) => TSelected
+): TSelected => useSelectorBase<RootState, TSelected>(selector);
