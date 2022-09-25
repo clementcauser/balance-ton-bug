a5e26956fb79668f74fa4ca20003d0a3520a3a1a
Clément Causer <34310857+clementcauser@users.noreply.github.com>
2022-09-25 15:31:52 +0200

    🚀  Setup Firebase (Firestore / Auth / Functions)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,10 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../../styles/Home.module.css";
+import { firebaseApp } from "../configuration/firebase";
 
 const Home: NextPage = () => {
+  console.log(firebaseApp);
   return (
     <div className={styles.container}>
       <Head>
