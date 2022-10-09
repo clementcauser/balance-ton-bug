369b5717314984fe7ace8df0908b4c0e263e94a3
Clément Causer <ccauser@simplonprod.co>
2022-10-09 16:15:56 +0200

    update firebase configuration

--- src/configuration/firebase.ts
+++ src/configuration/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,3 +17,4 @@ const firebaseConfig = {
 export const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(firebaseApp);
 export const firestore = getFirestore(firebaseApp);
+export const googleProvider = new GoogleAuthProvider();
